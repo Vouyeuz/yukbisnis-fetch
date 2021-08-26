750bf095f6e7cc542085012e4acfb849460f04ea
Vouyeuz <fahrizal.indi@gmail.com>
2021-08-27 02:15:28 +0700

    2:14 am || css responsiveness

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 fetch(`https://randomuser.me/api/`)
-  .then((res) => res.json())
+  .then(res => res.json())
   .then((res) => {
-    const users = res.results[0];
-    console.log(users);
+    const user = res.results[0];
+    console.log(user);
 
-    const card = showCards(users);
+    const card = showCards(user);
 
     const usersContainer = document.querySelector(".card");
     usersContainer.innerHTML = card;
   });
 
-function showCards(users) {
+// fetch(`https://randomuser.me/api/`)
+//   .then(res => res.json())
+//   .then((res) => {
+//     const nextUser = res.results[0];
+
+//     console.log(nextUser);
+//   })
+
+
+
+function showCards(user) {
   return `<div class="card__details">
-  <img
-    class="card__userPhoto"
-    src="${users.picture.large}"
-  />
-  <p class="card__userIntro">Hi, My name is</p>
-  <p class="card__userName">${users.name.first}</p>
-</div>`;
+            <div class="card__userPhoto">
+              <a href="/" class="card__nextUser">Next</a>
+              <img src="${user.picture.large}" />
+            </div>
+            <p class="card__userIntro">Hi, My name is</p>
+            <p class="card__userName">${user.name.first} ${user.name.last}</p>
+          </div>`;
 }
