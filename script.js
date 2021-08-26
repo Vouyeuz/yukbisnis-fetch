03acf3e6a994b8798caff62b79113728b9489557
Vouyeuz <fahrizal.indi@gmail.com>
2021-08-26 22:58:49 +0700

    half-done fetching users data

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-// $.ajax({
-//     url: 'https://randomuser.me/api/',
-//     dataType: 'json',
-//     success: function(data) {
-//       console.log(data);
-//     }
-//   });
-
-//!email mas yudi
-// yudi@yukbisnis.com
-
 fetch(`https://randomuser.me/api/`)
   .then((res) => res.json())
   .then((res) => {
-    const users = res;
+    const users = res.results[0];
     console.log(users);
-});
+
+    const card = showCards(users);
+
+    const usersContainer = document.querySelector(".card");
+    usersContainer.innerHTML = card;
+  });
+
+function showCards(users) {
+  return `<div class="card__details">
+  <img
+    class="card__userPhoto"
+    src="${users.picture.large}"
+  />
+  <p class="card__userIntro">Hi, My name is</p>
+  <p class="card__userName">${users.name.first}</p>
+</div>`;
+}
