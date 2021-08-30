70f618e6648eacbb82ea97b923dc4066e7fc089d
Vouyeuz <fahrizal.indi@gmail.com>
2021-08-30 16:12:17 +0700

    4:11 pm || fetch user details

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// client-side rendering 
+// client-side rendering
 fetch(`https://randomuser.me/api/`)
-  .then(res => res.json())
+  .then((res) => res.json())
   .then((res) => {
     const user = res.results[0];
-    // console.log(user);
+    console.log(user);
 
     const card = showCards(user);
 
@@ -12,11 +12,11 @@ fetch(`https://randomuser.me/api/`)
   });
 
 // server-side rendering while next link clicked (ajax)
-document.addEventListener("click", async function(e) {
-  if(e.target.matches(".card__nextUser")) {
+document.addEventListener("click", async function (e) {
+  if (e.target.matches(".card__nextUser")) {
     const newUser = await fetch(`https://randomuser.me/api/`)
-    .then(res => res.json())
-    .then((res) => res.results[0]);
+      .then((res) => res.json())
+      .then((res) => res.results[0]);
     // console.log(newUser);
 
     const nextCard = showCards(newUser);
@@ -28,9 +28,6 @@ document.addEventListener("click", async function(e) {
 
 // infinite scroll - ongoing!
 
-
-
-
 // DOM
 function showCards(user) {
   return `<div class="card__details">
@@ -40,5 +37,15 @@ function showCards(user) {
             </div>
             <p class="card__userIntro">Hi, My name is</p>
             <p class="card__userName">${user.name.first} ${user.name.last}</p>
+            <br><br><br>
+            <p class="card__userIntro">Here's my email</p>
+            <p class="card__userName" style="text-transform: lowercase">${user.email}</p>
+            <br>
+            <p class="card__userIntro">Call me through</p>
+            <p class="card__userName">${user.phone}</p>
+            <br>
+            <p class="card__userIntro">This is where I live</p>
+            <p class="card__userName">${user.location.street.name} ${user.location.street.number}, ${user.location.city}, ${user.location.country}</p>
+
           </div>`;
 }
